967b21f03d62f62b93dd01e2d76ef03668a7e1e4
Joyce <joyce0x611@gmail.com>
2019-08-07 20:09:34 -0700

    Updates

---
+++ precache-manifest.b64276d2c130bfa1c693198493d665e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd33a1a1aeff04e6aea3278c9a44aad",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "c411c150d8cb4cf17baa",
+    "url": "/reacttest/static/css/main.d7f1d447.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reacttest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c411c150d8cb4cf17baa",
+    "url": "/reacttest/static/js/main.bc3bfed1.chunk.js"
+  },
+  {
+    "revision": "db7600c47e6e7907c405",
+    "url": "/reacttest/static/js/runtime~main.492d25e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
