873746854280d27db94681ae85ecea03ea7d0f66
Joyce <joyce0x611@gmail.com>
2019-08-16 17:55:55 -0700

    Updates

---
+++ precache-manifest.7610352c128dd5cf6907483c13183eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf7c164ca4ce5df4a5639b4f65941e2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "66f62b90479680a22840",
+    "url": "/my-app/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "235bd326586ccc4fdee1",
+    "url": "/my-app/static/css/main.15d065d7.chunk.css"
+  },
+  {
+    "revision": "66f62b90479680a22840",
+    "url": "/my-app/static/js/2.cfd9d463.chunk.js"
+  },
+  {
+    "revision": "235bd326586ccc4fdee1",
+    "url": "/my-app/static/js/main.27d1274a.chunk.js"
+  },
+  {
+    "revision": "a1d6f7139752f47f6a5e",
+    "url": "/my-app/static/js/runtime~main.fe2b4f92.js"
+  }
+]);
