02041e8ce0e4747a53cea2869a0b682c0ae19d2b
Joyce <joyce0x611@gmail.com>
2019-08-20 15:47:49 -0700

    Updates

---
+++ precache-manifest.82425863cf17ef0295f6fa95b9a3ae63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae183bf3cf01900a6fada2c3d8d7560b",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "5f9c56be97290e8e4c70",
+    "url": "/reacttest/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "ef6228f94724af90feef",
+    "url": "/reacttest/static/css/main.383ab723.chunk.css"
+  },
+  {
+    "revision": "5f9c56be97290e8e4c70",
+    "url": "/reacttest/static/js/2.e68d8f4e.chunk.js"
+  },
+  {
+    "revision": "ef6228f94724af90feef",
+    "url": "/reacttest/static/js/main.07c63f7e.chunk.js"
+  },
+  {
+    "revision": "c8925b60759de80b4c51",
+    "url": "/reacttest/static/js/runtime~main.67d9c665.js"
+  },
+  {
+    "revision": "931cb1a56c1be27a566e0e851a798176",
+    "url": "/reacttest/static/media/grandeC.931cb1a5.jpg"
+  }
+]);
